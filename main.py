abb900da221dc467434f6f0b1276598f9d164a2b
Karan C <kc84273@gmail.com>
2026-09-18 13:42:02 +0530

    fixed a couple bugs regarding selection of country; added key metrics on top (dynamically changes with user selection); added 2 new columns with graphs to show valuable insights

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as pt
+import plotly.express as px
 import numpy as np
 
 st.set_page_config(page_title='Netlix Dashboard', layout='wide')
@@ -25,9 +25,35 @@ relyear = st.sidebar.slider(label='Release Year', min_value=df['release_year'].m
 #filtering data
 fil_df = df[df['type'].isin(content_type)]
 if country:
-    fil_df = fil_df[fil_df['country'].apply(lambda x: any(c.strip() in country for c in x.split(',')))]
+    fil_df = fil_df[fil_df['country'].apply(lambda x: any(c.strip() in country for c in x.split(',')) if isinstance(x, str) else False)] 
+    #fil_df = fil_df[fil_df['country'].isin(country)] ^^ the above checks for movies/tv shows that are in multiple countries
 if rating:
     fil_df = fil_df[fil_df['rating'].isin(rating)]
 if relyear:
     fil_df = fil_df[(fil_df['release_year'] >= relyear[0]) & (fil_df['release_year'] <= relyear[1])]
 
+#key metrics
+col1, col2, col3, col4 = st.columns(4)
+total_titles = len(fil_df)
+total_country = len(country)
+total_movie = len(fil_df[fil_df['type']=='Movie'])
+total_shows = len(fil_df[fil_df['type']=='TV Show'])
+col1.metric('Total Titles', total_titles)
+col2.metric('Movies', total_movie)
+col3.metric('TV Shows', total_shows)
+col4.metric('Countries', total_country)
+st.divider()
+
+col1,col2 = st.columns(2)
+with col1:
+    st.subheader('Movies vs TV Shows')
+    data = fil_df['type'].value_counts()
+    fig = px.pie(data, names=data.index, values=data.values, color_discrete_sequence=px.colors.sequential.Blues_r)
+    st.plotly_chart(fig, width='stretch')
+
+with col2:
+    st.subheader('Content Added Over Years')
+    data = fil_df['release_year'].value_counts().sort_index()
+    fig = px.line(data, x= data.index, y=data.values, markers=True)
+    fig.update_layout(xaxis_title='Years',yaxis_title='Titles')
+    st.plotly_chart(fig, width='stretch')
